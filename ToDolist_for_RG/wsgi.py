758dd9abd217b856cae4e83fb346c0dc625c9ca7
MorpheusFR <Creatorsk@gmail.com>
2017-08-17 19:34:47 +0300

    Initial commit

---
+++ ToDolist_for_RG/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ToDolist_for_RG project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ToDolist_for_RG.settings")
+
+application = get_wsgi_application()
