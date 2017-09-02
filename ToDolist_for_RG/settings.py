f3c8e21f36ec859df8155cecfe4a4230481906fd
shelartia <artemshelepun@gmail.com>
2017-09-02 17:49:38 +0300

    Update settings.py
    fdg

--- ToDolist_for_RG/settings.py
+++ ToDolist_for_RG/settings.py
@@ -122,17 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
+
 
 # List of finder classes that know how to find static files in
 # various locations.
